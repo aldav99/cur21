e698992a52f01bb39bf92f6a1e4b30d6778c63ac
aldav99 <aldav99@yandex.ru>
2018-04-17 05:57:24 +0300

    add VERSION

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module TestGuru
+    VERSION = '0.1.0' 
   class Application < Rails::Application
-    version = '0.1.0'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,7 +19,7 @@ module TestGuru
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.default_locale = :ru
+    config.i18n.default_locale = :ru
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
