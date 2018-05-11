cef08ada28020cfa8faa366fe13fa2a96d68207d
aldav99 <aldav99@yandex.ru>
2018-05-11 05:08:57 +0300

    REST add requirements

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,12 @@
 class QuestionsController < ApplicationController
 
-  before_action :find_test_by_question, only: %i[destroy]
+  before_action :find_test
   before_action :find_question, only: %i[show]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    test = find_test
-    result = test.questions.pluck(:id, :body)
+    result = @test.questions.pluck(:id, :body)
     render plain: result.join("\n")
   end
 
@@ -17,14 +16,11 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @test = find_test
     @question = @test.questions.build
   end
 
   def create
-    @test = find_test
-    @question = @test.questions.build(question_params)
-    @question.save
+    @question = @test.questions.create(question_params)
     render plain: @question.inspect
   end
 
@@ -37,11 +33,7 @@ class QuestionsController < ApplicationController
   private
 
   def find_test
-    Test.find(params[:test_id])
-  end
-
-  def find_test_by_question
-    @test = Test.joins(:questions).where(questions: {test_id: params[:id]}).first
+    @test = Test.find(params[:test_id])
   end
 
   def find_question
