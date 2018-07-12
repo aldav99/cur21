51c141a155d0c8fc9312bf6d620a6a88f915cf6a
aldav99 <aldav99@yandex.ru>
2018-07-12 06:01:05 +0300

    Разрешить Null для user.name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_022845) do
+ActiveRecord::Schema.define(version: 2018_07_12_025754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
