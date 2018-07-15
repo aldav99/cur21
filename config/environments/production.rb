3ae2f4211f91d2cfbb5a60996be763a56f4974d3
aldav99 <aldav99@yandex.ru>
2018-07-15 15:47:49 +0300

    новый heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host:  'al-testguru.herokuapp.com' }
+  config.action_mailer.default_url_options = { host:  'radiant-island-92476.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
